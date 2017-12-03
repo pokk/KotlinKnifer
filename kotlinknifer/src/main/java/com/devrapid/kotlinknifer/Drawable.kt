cb33fb4cce748f6388c1d29ef7684954745b02f5
Jieyi <pokkbaby@gmail.com>
2017-12-03 11:07:26 +0900

    Added the blur bitmap function.

--- kotlinknifer/src/main/java/com/devrapid/kotlinknifer/Drawable.kt
+++ kotlinknifer/src/main/java/com/devrapid/kotlinknifer/Drawable.kt
@@ -5,6 +5,10 @@ import android.graphics.Bitmap
 import android.graphics.Rect
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.ScaleDrawable
+import android.renderscript.Allocation
+import android.renderscript.Element
+import android.renderscript.RenderScript
+import android.renderscript.ScriptIntrinsicBlur
 import android.support.annotation.DrawableRes
 import android.support.v4.content.ContextCompat
 import android.support.v7.graphics.Palette
@@ -25,3 +29,27 @@ fun Context.scaledDrawable(@DrawableRes drawableId: Int, scaleWidth: Float,
 }
 
 fun Bitmap.palette() = Palette.from(this)
+
+fun Context.blurBitmap(image: Bitmap, radius: Float = 25f, scale: Float = 0.4f): Bitmap {
+    val width = Math.round(image.width * scale)
+    val height = Math.round(image.height * scale)
+    // Because of the blurring, we don't have to use original bitmap to blur. It's able to reduce cost.
+    val scaledBitmap = Bitmap.createScaledBitmap(image, width, height, false)
+    // Create a image for blurring.
+    val blurBitmap = Bitmap.createBitmap(scaledBitmap)
+    val rs = RenderScript.create(this)
+    // RenderScript doesn't use VM to allocate memory, we have to do it by ourselves.
+    val tmpIn = Allocation.createFromBitmap(rs, scaledBitmap)
+    // The created Allocation is empty actually, copyTo() is necessary to fill the date.
+    val tmpOut = Allocation.createFromBitmap(rs, blurBitmap)
+
+    ScriptIntrinsicBlur.create(rs, Element.U8_4(rs)).apply {
+        setRadius(radius)
+        setInput(tmpIn)
+        forEach(tmpOut)
+    }
+
+    tmpOut.copyTo(blurBitmap)
+
+    return blurBitmap
+}
