0a0c6badb2ded79c907b580911530946815bad32
Jieyi <pokkbaby@gmail.com>
2018-01-23 19:32:42 +0900

    Fixed the keyboard methods.

--- kotlinknifer/src/main/java/com/devrapid/kotlinknifer/Keyboard.kt
+++ kotlinknifer/src/main/java/com/devrapid/kotlinknifer/Keyboard.kt
@@ -3,6 +3,7 @@
 package com.devrapid.kotlinknifer
 
 import android.content.Context
+import android.graphics.Rect
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
@@ -10,7 +11,6 @@ import android.view.inputmethod.InputMethodManager
  * @author  jieyi
  * @since   11/8/17
  */
-
 inline fun View.hideSoftKeyboard() = run {
     clearFocus()
     (context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
@@ -26,11 +26,29 @@ inline fun View.showSoftKeyboard() =
             .showSoftInput(this, InputMethodManager.SHOW_FORCED)
     }
 
+fun isShowingSoftKeyboard(rootView: View): Boolean {
+    val softKeyboardHeight = 100
+    val r = Rect()
+
+    return rootView.run {
+        getWindowVisibleDisplayFrame(r)
+
+        val heightDiff = bottom - r.bottom
+
+        heightDiff > softKeyboardHeight * resources.displayMetrics.density
+    }
+}
+
 inline fun Context.toggleSoftKeyboard() =
     (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
         .toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
 
-inline fun Context.isHidingSoftKeyBoard() = !isShowingSoftKeyBoard()
-
-inline fun Context.isShowingSoftKeyBoard() =
-    (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).isAcceptingText
+// FIXME(jieyi): 2018/01/23 Those functions don't work well.
+//inline fun Context.hideSoftKeyboard() = if (isShowingSoftKeyboard()) toggleSoftKeyboard() else Unit
+//
+//inline fun Context.showSoftKeyboard() = if (isHidingSoftKeyboard()) toggleSoftKeyboard() else Unit
+//
+//inline fun Context.isHidingSoftKeyboard() = !isShowingSoftKeyboard()
+//
+//inline fun Context.isShowingSoftKeyboard() =
+//    (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).isAcceptingText
