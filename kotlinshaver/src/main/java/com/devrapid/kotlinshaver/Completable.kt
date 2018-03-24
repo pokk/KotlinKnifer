71fd06fb44d32110db157ca24bbe4a01356a6b46
Jieyi <pokkbaby@gmail.com>
2018-03-25 02:34:10 +0900

    Added the completable and single plugin.

--- kotlinshaver/src/main/java/com/devrapid/kotlinshaver/Completable.kt
+++ kotlinshaver/src/main/java/com/devrapid/kotlinshaver/Completable.kt
@@ -1,49 +1,40 @@
 package com.devrapid.kotlinshaver
 
-import io.reactivex.Observer
+import io.reactivex.CompletableObserver
 import io.reactivex.disposables.Disposable
 
 /**
- * RxJava2 extension tool.
- *
- * @author  jieyi
- * @since   9/8/17
+ * @author  Jieyi Wu
+ * @since   2018/03/25
  */
-fun <T> observer(): ObserverPlugin<T> = ObserverPlugin()
+fun <T> completable(): CompletablePlugin = CompletablePlugin()
 
-fun <T> observer(
+fun <T> completable(
     onError: (Throwable) -> Unit = {},
     onComplete: () -> Unit = {},
-    onSubscribe: (Disposable) -> Unit = {},
-    onNext: (T) -> Unit = {}
-) =
-    ObserverPlugin<T>().apply {
-        onSubscribe(onSubscribe)
-        onNext(onNext)
-        onComplete(onComplete)
-        onError(onError)
-    }
+    onSubscribe: (Disposable) -> Unit = {}
+) = CompletablePlugin().apply {
+    onSubscribe(onSubscribe)
+    onComplete(onComplete)
+    onError(onError)
+}
 
-class ObserverPlugin<T> : Observer<T> {
+class CompletablePlugin : CompletableObserver {
     private var _onSubscribe: ((Disposable) -> Unit)? = null
-    private var _onNext: ((T) -> Unit)? = null
     private var _onComplete: (() -> Unit)? = null
     private var _onError: ((Throwable) -> Unit)? = null
 
     override fun onSubscribe(d: Disposable) = this._onSubscribe?.invoke(d) ?: Unit
-    override fun onNext(t: T) = this._onNext?.invoke(t) ?: Unit
     override fun onComplete() = this._onComplete?.invoke() ?: Unit
     override fun onError(e: Throwable) = this._onError?.invoke(e) ?: Unit
 
     fun onSubscribe(onSubscribeFun: (Disposable) -> Unit) = apply { _onSubscribe = onSubscribeFun }
-    fun onNext(onNextFun: (t: T) -> Unit) = apply { _onNext = onNextFun }
     fun onComplete(onCompleteFun: () -> Unit) = apply { _onComplete = onCompleteFun }
     fun onError(onErrorFun: (t: Throwable) -> Unit) = apply { _onError = onErrorFun }
 
-    fun ObserverPlugin<T>.copy() = ObserverPlugin<T>().apply {
+    fun CompletablePlugin.copy(): CompletablePlugin = CompletablePlugin().apply {
         // TODO(jieyi): 8/14/17 Those functions are the same pointer as before. Duplicate the functions!
         _onSubscribe = this@copy._onSubscribe
-        _onNext = this@copy._onNext
         _onComplete = this@copy._onComplete
         _onError = this@copy._onError
     }
