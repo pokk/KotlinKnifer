0064b23ab13996c1e178f657729bec42163d52ce
Jieyi <pokkbaby@gmail.com>
2019-04-17 18:22:48 +0900

    Feature: added the encrypt and updated `kotlin`, `coroutine` version 1.3.30, 1.2.0.

---
+++ kotlinknifer/src/main/java/com/devrapid/kotlinknifer/encrypt/AESEncrypt.kt
@@ -0,0 +1,40 @@
+package com.devrapid.kotlinknifer.encrypt
+
+import android.util.Base64
+import java.security.MessageDigest
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+
+object AESEncrypt {
+    fun encrypt(data: String, key: String) =
+        Base64.encodeToString(ase_256_encrypt(data, key), Base64.DEFAULT)
+
+    fun decrypt(encrypted: String, key: String) =
+        String(ase_256_decrypt(Base64.decode(encrypted, Base64.DEFAULT), key))
+
+    private fun ase_256_encrypt(data: String, key: String): ByteArray {
+        val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+        val keySpec = SecretKeySpec(getCipherKey(key), "SHA-256")
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, getIvSpec())
+        return cipher.doFinal(data.toByteArray())
+    }
+
+    private fun ase_256_decrypt(dataByte: ByteArray, key: String): ByteArray {
+        val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+        val keySpec = SecretKeySpec(getCipherKey(key), "SHA-256")
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, getIvSpec())
+        return cipher.doFinal(dataByte)
+    }
+
+    private fun getCipherKey(key: String): ByteArray {
+        val digest = MessageDigest.getInstance("SHA-256")
+        digest.update(key.toByteArray())
+        val keyBytes = ByteArray(32)
+        System.arraycopy(digest.digest(), 0, keyBytes, 0, keyBytes.size)
+
+        return keyBytes
+    }
+
+    private fun getIvSpec() = IvParameterSpec(byteArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+}
--- kotlinshaver/src/main/java/com/devrapid/kotlinshaver/Coroutine.kt
+++ kotlinshaver/src/main/java/com/devrapid/kotlinshaver/Coroutine.kt
@@ -22,6 +22,8 @@ inline fun gLaunch(
 
 inline fun ui(noinline block: suspend CoroutineScope.() -> Unit) = CoroutineScope(Main.immediate).launch(block = block)
 
+inline fun uiLate(noinline block: suspend CoroutineScope.() -> Unit) = CoroutineScope(Main).launch(block = block)
+
 inline fun bkg(noinline block: suspend CoroutineScope.() -> Unit) = CoroutineScope(Default).launch(block = block)
 
 inline fun io(noinline block: suspend CoroutineScope.() -> Unit) = CoroutineScope(IO).launch(block = block)
@@ -37,14 +39,18 @@ inline fun <T> gAsync(
 inline fun <T> uiAsync(noinline block: suspend CoroutineScope.() -> T) =
     CoroutineScope(Main.immediate).async(block = block)
 
+inline fun <T> uiLateAsync(noinline block: suspend CoroutineScope.() -> T) = CoroutineScope(Main).async(block = block)
+
 inline fun <T> bkgAsync(noinline block: suspend CoroutineScope.() -> T) = CoroutineScope(Default).async(block = block)
 
 inline fun <T> ioAsync(noinline block: suspend CoroutineScope.() -> T) = CoroutineScope(IO).async(block = block)
 
 // ----------------------- with context
 
-suspend fun <T> uiSwitch(block: suspend CoroutineScope.() -> T) = withContext(Main.immediate, block)
+suspend inline fun <T> uiSwitch(noinline block: suspend CoroutineScope.() -> T) = withContext(Main.immediate, block)
 
-suspend fun <T> bkgSwitch(block: suspend CoroutineScope.() -> T) = withContext(Default, block)
+suspend inline fun <T> uiLateSwitch(noinline block: suspend CoroutineScope.() -> T) = withContext(Main, block)
 
-suspend fun <T> ioSwitch(block: suspend CoroutineScope.() -> T) = withContext(IO, block)
+suspend inline fun <T> bkgSwitch(noinline block: suspend CoroutineScope.() -> T) = withContext(Default, block)
+
+suspend inline fun <T> ioSwitch(noinline block: suspend CoroutineScope.() -> T) = withContext(IO, block)
