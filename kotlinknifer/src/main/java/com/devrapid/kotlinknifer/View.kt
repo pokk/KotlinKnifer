2943e3e660ca6d01914a45bfd0928bc5ed2821f2
Jieyi <pokkbaby@gmail.com>
2019-05-25 22:15:15 +0900

    Fix: change status bar color and bumped into the newest version.

--- kotlinknifer/src/main/java/com/devrapid/kotlinknifer/View.kt
+++ kotlinknifer/src/main/java/com/devrapid/kotlinknifer/View.kt
@@ -106,6 +106,8 @@ internal inline fun Activity.setStatusBarColorBy(block: Window.() -> Unit) {
             clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
             addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
             block()
+            // For not opaque(transparent) color.
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         }
     else {
         TODO("Don't support the sdk version is less than 21 yet.")
